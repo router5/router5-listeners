e68989692ab6ff0832cc67f10331de458a380e75
Thomas Roch <thomas.c.roch@gmail.com>
2016-01-17 10:11:47 +0000

    chore: update v2 dist

--- dist/commonjs/index.js
+++ dist/commonjs/index.js
@@ -11,87 +11,94 @@ var _router2 = _interopRequireDefault(_router);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var pluginName = 'LISTENERS';
+var defaultOptions = {
+    autoCleanUp: true
+};
 
-function listenersPlugin(router) {
-    var listeners = {};
+function listenersPlugin() {
+    var options = arguments.length <= 0 || arguments[0] === undefined ? defaultOptions : arguments[0];
 
-    var removeListener = function removeListener(name, cb) {
-        if (cb) {
-            if (listeners[name]) listeners[name] = listeners[name].filter(function (callback) {
-                return callback !== cb;
-            });
-        } else {
-            listeners[name] = [];
-        }
-        return router;
-    };
+    return function plugin(router) {
+        var listeners = {};
 
-    var addListener = function addListener(name, cb, replace) {
-        var normalizedName = name.replace(/^(\*|\^|=)/, '');
+        var removeListener = function removeListener(name, cb) {
+            if (cb) {
+                if (listeners[name]) listeners[name] = listeners[name].filter(function (callback) {
+                    return callback !== cb;
+                });
+            } else {
+                listeners[name] = [];
+            }
+            return router;
+        };
 
-        if (normalizedName && !/^\$/.test(name)) {
-            var segments = router.rootNode.getSegmentsByName(normalizedName);
-            if (!segments) console.warn('No route found for ' + normalizedName + ', listener might never be called!');
-        }
+        var addListener = function addListener(name, cb, replace) {
+            var normalizedName = name.replace(/^(\*|\^|=)/, '');
 
-        if (!listeners[name]) listeners[name] = [];
-        listeners[name] = (replace ? [] : listeners[name]).concat(cb);
+            if (normalizedName && !/^\$/.test(name)) {
+                var segments = router.rootNode.getSegmentsByName(normalizedName);
+                if (!segments) console.warn('No route found for ' + normalizedName + ', listener might never be called!');
+            }
 
-        return router;
-    };
+            if (!listeners[name]) listeners[name] = [];
+            listeners[name] = (replace ? [] : listeners[name]).concat(cb);
 
-    router.addListener = function (cb) {
-        return addListener('*', cb);
-    };
-    router.removeListener = function (cb) {
-        return removeListener('*', cb);
-    };
+            return router;
+        };
 
-    router.addNodeListener = function (name, cb) {
-        return addListener('^' + name, cb, true);
-    };
-    router.removeNodeListener = function (name, cb) {
-        return removeListener('^' + name, cb);
-    };
+        router.addListener = function (cb) {
+            return addListener('*', cb);
+        };
+        router.removeListener = function (cb) {
+            return removeListener('*', cb);
+        };
 
-    router.addRouteListener = function (name, cb) {
-        return addListener('=' + name, cb);
-    };
-    router.removeRouteListener = function (name, cb) {
-        return removeListener('=' + name, cb);
-    };
+        router.addNodeListener = function (name, cb) {
+            return addListener('^' + name, cb, true);
+        };
+        router.removeNodeListener = function (name, cb) {
+            return removeListener('^' + name, cb);
+        };
 
-    function invokeListeners(name, toState, fromState) {
-        (listeners[name] || []).forEach(function (cb) {
-            return cb(toState, fromState);
-        });
-    }
+        router.addRouteListener = function (name, cb) {
+            return addListener('=' + name, cb);
+        };
+        router.removeRouteListener = function (name, cb) {
+            return removeListener('=' + name, cb);
+        };
 
-    function onTransitionSuccess(toState, fromState, opts) {
-        var _transitionPath = (0, _router2.default)(toState, fromState);
-
-        var intersection = _transitionPath.intersection;
-        var toDeactivate = _transitionPath.toDeactivate;
-
-        var intersectionNode = opts.reload ? '' : intersection;
-        var name = toState.name;
-
-        if (router.options.autoCleanUp) {
-            toDeactivate.forEach(function (name) {
-                return removeListener('^' + name);
+        function invokeListeners(name, toState, fromState) {
+            (listeners[name] || []).forEach(function (cb) {
+                return cb(toState, fromState);
             });
         }
 
-        invokeListeners('^' + intersection, toState, fromState);
-        invokeListeners('=' + name, toState, fromState);
-        invokeListeners('*', toState, fromState);
-    }
+        function onTransitionSuccess(toState, fromState, opts) {
+            var _transitionPath = (0, _router2.default)(toState, fromState);
 
-    function flush() {
-        listeners = {};
-    }
+            var intersection = _transitionPath.intersection;
+            var toDeactivate = _transitionPath.toDeactivate;
 
-    return { name: pluginName, init: init, onTransitionSuccess: onTransitionSuccess, flush: flush, listeners: listeners };
+            var intersectionNode = opts.reload ? '' : intersection;
+            var name = toState.name;
+
+            if (options.autoCleanUp) {
+                toDeactivate.forEach(function (name) {
+                    return removeListener('^' + name);
+                });
+            }
+
+            invokeListeners('^' + intersection, toState, fromState);
+            invokeListeners('=' + name, toState, fromState);
+            invokeListeners('*', toState, fromState);
+        }
+
+        function flush() {
+            listeners = {};
+        }
+
+        return { name: pluginName, onTransitionSuccess: onTransitionSuccess, flush: flush, listeners: listeners };
+    };
 }
 
 exports.default = listenersPlugin;
